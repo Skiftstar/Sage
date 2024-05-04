c14a499c8bece05b1a9556c6c1d79e1c0e63eb8b
Yukami <46356129+Skiftstar@users.noreply.github.com>
2024-05-04 23:20:07 +0200

    init

---
+++ Discord-Bot/src/DiscordBot/bot.ts
@@ -0,0 +1,69 @@
+import {
+  EmbedBuilder,
+  TextChannel,
+  ChatInputCommandInteraction,
+  GuildBasedChannel
+} from 'discord.js'
+import { getConfigValue } from '../Config/config'
+import { client } from '..'
+
+export const startBot = () => {
+  client.login(getConfigValue('token'))
+}
+
+export const sendMessage = (channel: TextChannel, message: string) => {
+  channel.send(message)
+}
+
+export const replyEmbed = (
+  interaction: ChatInputCommandInteraction,
+  embed: EmbedBuilder
+) => {
+  interaction.reply({ embeds: [embed] })
+}
+
+export const bulkDelete = (channel: TextChannel) => {
+  if (channel === undefined) {
+    return sendError(`BulkDelete: Channel is undefined`)
+  }
+  channel.bulkDelete(20).catch((error) => {
+    sendError(error)
+  })
+}
+
+export const sendError = (error: string, additionalData?: any) => {
+  console.log(error, additionalData)
+}
+
+export const sendEmbed = (channel: TextChannel, embed: EmbedBuilder) => {
+  if (channel === undefined) {
+    return sendError('sendEmbed: Channel undefined')
+  }
+  channel.send({ embeds: [embed] }).catch((err) => {
+    sendError('Error while sending embed, see logs', err)
+  })
+}
+
+export const getChannelById = (channelId: string) => {
+  const guildId = getConfigValue('guildId')
+
+  return client.guilds.cache
+    .get(guildId)
+    ?.channels.cache.get(channelId) as TextChannel
+}
+
+export const getChannelsOfGuild = (guildId: string) => {
+  const guild = client.guilds.cache.get(guildId)
+
+  const channels = Array.from(guild?.channels.cache.values() || [])
+  return channels
+}
+
+export const getGuild = (guildId: string) => {
+  return client.guilds.cache.get(guildId)
+}
+
+export const getUsersOfGuild = (guildId: string) => {
+  const guild = client.guilds.cache.get(guildId)
+  return Array.from(guild?.members.cache.values() || [])
+}
